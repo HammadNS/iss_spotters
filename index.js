4e83615a6ba6984b2d9035ff2fee5744959cfc35
hammadNS96 <hammad96@outlook.com>
2020-03-12 17:06:38 +0000

    Completed iss-explorer-two

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+
+// fetchCordsByIP('162.245.144.188', (error, coords) => {
+// if (error) {
+// console.log("It didn't work!", error);
+// return;
+// }
+
+// console.log("It worked! Returned Coords", coords);
+
+// });
